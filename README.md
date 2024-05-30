bbf3d938f9f2c3b7fe51abb818faf6de4c5e60b1
Wijnand Karsens <wijnand@karsens.com>
2024-05-30 08:30:29 +0200

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# cloc-everywhere
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run cloc-everywhere.cli.ts
+```
+
+This project was created using `bun init` in bun v1.1.10. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
---
+++ cloc-everywhere.cli.ts
@@ -0,0 +1,44 @@
+#!/usr/bin/env bun
+
+import { $ } from "bun";
+import { readdirSync } from "node:fs";
+import path from "node:path";
+const [basePath] = process.argv.slice(2);
+
+$.nothrow();
+
+const absoluteFolderPath =
+  !basePath || basePath.startsWith(".")
+    ? path.join(process.cwd(), basePath || ".")
+    : basePath;
+
+const folderPaths = readdirSync(absoluteFolderPath, { withFileTypes: true })
+  .filter((x) => x.isDirectory())
+  .map((x) => path.join(absoluteFolderPath, x.name));
+
+await Promise.all(
+  folderPaths.map(async (p) => {
+    const result:
+      | {
+          SUM:
+            | { nFiles: number; blank: number; comment: number; code: number }
+            | undefined;
+        }
+      | undefined = await $`cloc ${p}/src ${p}/app --json`.json();
+
+    if (!result) {
+      return;
+    }
+
+    const sum = result.SUM;
+    if (!sum) {
+      return;
+    }
+    const { blank, code, comment, nFiles } = sum;
+
+    const totalLines = blank + code + comment;
+    const comments = `${totalLines} LOC over ${nFiles} files`;
+    console.log(p, comments);
+    await $`osascript -e 'on run {f, c}' -e 'tell app "Finder" to set comment of (POSIX file f as alias) to c' -e end "${p}" "${comments}"`.quiet();
+  }),
+);
